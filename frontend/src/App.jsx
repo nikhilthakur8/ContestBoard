8fff722e1df3dbcf7b9a62c665d384e4f7de546d
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-06-04 13:23:50 +0530

    fixed bug and added analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Home } from "./components/Home/Home";
 import { NavbarDemo } from "./components/NavBar/NavBar";
-
+import { Analytics } from "@vercel/analytics/react";
 export const App = () => {
 	return (
 		<div className="min-h-screen bg-gray-950 lg:px-12 px-5 text-gray-200">
 			<NavbarDemo />
 			<Home />
+			<Analytics />
 		</div>
 	);
 };
