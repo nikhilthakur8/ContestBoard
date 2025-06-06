75b7a47a1c70504164675ef08c095050c5e72599
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-06-06 15:20:44 +0530

    ui change

--- frontend/src/components/Chats/ChatPage.jsx
+++ frontend/src/components/Chats/ChatPage.jsx
@@ -182,29 +182,24 @@ export const ChatPage = () => {
 function ChatHeader({ roomData }) {
 	const navigate = useNavigate();
 	return (
-		<div className="px-5 flex items-center gap-4 py-3 bg-gray-950">
+		<div className="px-2 flex items-center text-base md:text-xl gap-3 py-3 bg-gray-950">
 			<button
-				className="cursor-pointer hover:bg-gray-800 border hover:border-gray-700 border-transparent rounded-full p-1"
+				className="cursor-pointer hover:bg-gray-800 border hover:border-gray-700 border-transparent rounded-full p-1 flex"
 				title="Back to Discussion"
 				onClick={() => window.history.back() || navigate("/discussion")}
 			>
-				<ArrowLeft />
+				<ArrowLeft className="size-6" />
 			</button>
-			<div className="w-8 h-8 md:w-10 md:h-10 p-1 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600">
+			<div className="w-10 h-10 p-1 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600">
 				<img
-					src={
-						images[roomData?.platform] ||
-						"https://api.dicebear.com/7.x/pixel-art/svg?seed=happyuser"
-					}
+					src={images[roomData?.platform] || images["LeetCode"]}
 					alt="platform logo"
-					className="w-5 h-5 md:w-6 md:h-6 object-contain"
+					className="w-7 h-7 object-contain"
 				/>
 			</div>
-			<span className="text-xs sm:text-sm md:text-base xl:text-lg font-semibold">
-				{roomData?.name}
-			</span>
+			<span className="font-semibold">{roomData?.name}</span>
 			<ExternalLinkIcon
-				className="text-sky-500 size-4 md:size-6 transition hover:scale-120 hover:text-sky-400 cursor-pointer"
+				className="text-sky-500 size-6 transition hover:scale-120 hover:text-sky-400 cursor-pointer"
 				onClick={() => window.open(roomData.link, "_blank")}
 				title="Go to Contest Link"
 			/>
@@ -223,7 +218,7 @@ function ChatMessage({
 }) {
 	return (
 		<div className="overflow-y-auto scrollbar-hide h-full  py-5">
-			<div className="flex flex-col text-xs xl:text-sm">
+			<div className="flex flex-col text-sm md:text-base">
 				{messages.map((message) => (
 					<Message
 						key={message.id}
@@ -242,9 +237,9 @@ function ChatMessage({
 
 function ChatInput({ handleSubmit, inputRef, replyTo, setReplyTo }) {
 	return (
-		<div className="w-full text-sm md:text-base flex  flex-col border-t border-gray-700">
+		<div className="w-full text-md md:text-base flex  flex-col border-t border-gray-700">
 			{replyTo.chatRefId && (
-				<div className="flex items-center justify-between rounded-t-md text-xs bg-gray-700">
+				<div className="flex border-l-6  border-green-500 items-center justify-between rounded-t-md text-xs md:text-sm bg-gray-700">
 					<div className="px-2 md:px-5 py-1 max-h-16 md:max-h-20 overflow-hidden  w-full">
 						<p className="text-gray-500 text-xs">
 							~ {replyTo.reply.name || "User"}
@@ -267,7 +262,7 @@ function ChatInput({ handleSubmit, inputRef, replyTo, setReplyTo }) {
 			<div className="w-full">
 				<form
 					onSubmit={handleSubmit}
-					className="text-xs sm:text-sm md:text-base inline-flex w-full"
+					className="text-base md:text-lg inline-flex w-full"
 				>
 					<textarea
 						placeholder="Type message..."
@@ -276,7 +271,7 @@ function ChatInput({ handleSubmit, inputRef, replyTo, setReplyTo }) {
 						rows={1}
 						spellCheck={"false"}
 						name={"message"}
-						className="w-full px-5 py-3 h-auto max-h-32 leading-snug resize-none overflow-y-auto outline-none  break-words scrollbar-hide "
+						className="w-full px-5 py-3 h-12 max-h-32 leading-snug resize-none overflow-y-auto outline-none  break-words scrollbar-hide"
 						onInput={(e) => {
 							e.target.style.height = "auto";
 							const scrollHeight = e.target.scrollHeight;
@@ -286,10 +281,10 @@ function ChatInput({ handleSubmit, inputRef, replyTo, setReplyTo }) {
 						}}
 					/>
 					<button
-						className="rounded-full px-2 md:px-10 cursor-pointer"
+						className="rounded-full px-4 md:px-10 cursor-pointer"
 						type="submit"
 					>
-						<SendHorizontalIcon className="size-4 md:size-6" />
+						<SendHorizontalIcon className="size-6" />
 					</button>
 				</form>
 			</div>
--- frontend/src/components/Discussion/Discussion.jsx
+++ frontend/src/components/Discussion/Discussion.jsx
@@ -16,7 +16,7 @@ export const Discussion = () => {
 	}, []);
 
 	return (
-		<div className="pt-20 md:pt-28 py-10 px-12 min-h-screen text-gray-300 rounded-md">
+		<div className="py-20 md:py-28  min-h-screen text-gray-300 rounded-md">
 			<h2 className="mb-6 text-3xl font-bold tracking-wider text-gray-400">
 				All Discussions ({discussionsData.total})
 			</h2>
@@ -24,53 +24,58 @@ export const Discussion = () => {
 				{discussionsData.documents.map((d) => (
 					<div
 						key={d.platform}
-						className="p-4 rounded-md border border-neutral-800/90 hover:scale-105 transform duration-300 cursor-pointer bg-gray-900 flex gap-7 items-center"
+						className="p-3 md:p-4 rounded-md border border-neutral-800/90 hover:scale-105 transform duration-300 cursor-pointer bg-gray-900 flex gap-3 md:gap-5 items-center"
 						onClick={() => navigate(`/discussion/${d.$id}`)}
 					>
-						<div className="w-10 h-10 bg-neutral-500/80 flex items-center justify-center rounded-full">
+						<ContestStatus
+							startTime={d.startTime}
+							endTime={d.endTime}
+						/>
+						<div className="w-10 shrink-0 h-10 bg-neutral-500/80 flex items-center justify-center rounded-full">
 							<img
 								src={images[d.platform]}
 								alt={d.platform}
-								className="object-contain w-2/3 h-2/3"
+								className="object-contain w-7 h-7"
 								title={d.platform}
 							/>
 						</div>
-						<div className="flex w-full justify-between ">
+						<div className="flex flex-col sm:flex-row w-full gap-2 justify-between ">
 							<div className="flex flex-col justify-center">
 								<p className="text-xs text-gray-300">
 									{d.type}
 								</p>
-								<h3 className="text-xl" title="platform">
+								<h3
+									className="text-base md:text-xl"
+									title="platform"
+								>
 									{d.name}
 								</h3>
-								<div className="text-gray-500">
-									Add any additional information here
+								<div className="text-gray-500 text-base break-all">
+									{d.platform} - {d.contestId}
 								</div>
 							</div>
-							<div className="flex gap-4 items-center text-sm">
-								<ContestStatus
-									startTime={d.startTime}
-									endTime={d.endTime}
-								/>
-								<div className="flex flex-col gap-1">
-									<DateTimeBlock
-										label="Start"
-										date={d.startTime}
-									/>
-									<TimeBlock
-										label="Duration"
-										value={`${d.duration / 3600} hours`}
-									/>
-									<DateTimeBlock
-										label="End"
-										date={d.endTime}
-									/>
+							<div className="flex gap-4 justify-between items-center text-xs md:text-sm">
+								<div className="flex flex-row items-center gap-4">
+									<div className="flex flex-col gap-1">
+										<DateTimeBlock
+											label="Start"
+											date={d.startTime}
+										/>
+										<TimeBlock
+											label="Duration"
+											value={`${d.duration / 3600} hours`}
+										/>
+										<DateTimeBlock
+											label="End"
+											date={d.endTime}
+										/>
+									</div>
 								</div>
 								<a
 									href={d.link}
 									target="_blank"
 									rel="noopener noreferrer"
-									className="text-blue-400 hover:underline"
+									className="text-blue-400 hover:underline hidden sm:block"
 									title={d.link}
 								>
 									<ExternalLink className="hover:text-green-500" />
@@ -105,7 +110,7 @@ const TimeBlock = ({ label, value }) => (
 	</div>
 );
 
-export const ContestStatus = ({ startTime, endTime }) => {
+export const ContestStatus = ({ startTime, endTime, className }) => {
 	const now = new Date();
 	const start = new Date(startTime);
 	const end = new Date(endTime);
@@ -113,7 +118,9 @@ export const ContestStatus = ({ startTime, endTime }) => {
 		now < start ? "bg-gray-400" : now > end ? "bg-red-400" : "bg-green-400";
 
 	return (
-		<div className="relative size-3 flex items-center justify-center rounded-full">
+		<div
+			className={`relative size-3  items-center justify-center rounded-full ${className}`}
+		>
 			<div
 				className={`absolute w-full h-full animate-ping ${status} rounded-full`}
 			/>
